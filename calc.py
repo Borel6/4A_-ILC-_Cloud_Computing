f675677066da7d70edcee8b0b556d83b65454aec
Borel6 <t.borel33@gmail.com>
2024-02-16 11:34:45 +0100

    implemtation de rabbitMQ

---
+++ calc.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import pika, sys, os 
+import json
+import redis
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+def main() :
+
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='calculs')
+
+    def callback(ch, method, properties, body):
+        message = json.loads(body)
+        operation = message['message']['op']
+        numA = message['message']['numA']
+        numB = message['message']['numB']
+        id = message['id']
+
+        if (operation == "add") :
+            C = int(numA) + int(numB)
+            r.set(id, C) 
+
+        print(f" [x] Received {body}")
+
+    channel.basic_consume(queue='calculs',
+                        auto_ack=True,
+                        on_message_callback=callback)
+
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- main.py
+++ main.py
@@ -2,20 +2,26 @@ from flask import Flask, render_template, request
 import json 
 
 import redis
+
+#!/usr/bin/env python
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='calculs')
+
+
+
 r = redis.Redis(host='localhost', port=6379, db=0)
-
-
 app = Flask(__name__)
 
 
 
-resultats = {1 : 5}
-
-
-@app.route("/display", methods=['GET'])
-def get_result():
-	if request.method == 'GET':
-		return str((resultats.items()))
+#@app.route("/display", methods=['GET'])
+#def get_result():
+#	if request.method == 'GET':
+#		return str((resultats.items()))
      
 #addition
 @app.route("/api/add/<A>/<B>", methods=["GET", "POST"])
@@ -24,11 +30,27 @@ def add(A,B):
         A = int(A)
         B = int(B)
         C = A + B 
-        id = r.dbsize() + 1
-        r.set(id, C) 
-        resultats[id] = C
+        id = r.dbsize + 1
+       # r.set(id, C) 
+        id = "calc" + str(id)
+        data = {
+        "id": id,
+        "message": {
+            "op": "add",
+            "numA": A,
+            "numB": B
+            }
+            }
+        json_data = json.dumps(data)
+        
 
-        return " a bien été ajouté au dictionnaire"
+        channel.basic_publish(exchange='',
+                        routing_key='calculs',
+                      body=json_data)
+                      
+        print(" [x] Sent 'Hello World!'")
+
+        return id 
 
 
 #soustraction
@@ -39,9 +61,6 @@ def subs(A,B):
         B = int(B)
         C = A - B 
         id = r.dbsize() + 1
-        r.set(id, C) 
-
-        resultats[id] = C
 
         return " a bien été ajouté au dictionnaire"
     
@@ -56,7 +75,6 @@ def div(A,B):
 
         C = A - B 
         id = r.dbsize() + 1
-        resultats[id] = C
         r.set(id, C) 
 
 
@@ -70,9 +88,9 @@ def multi(A,B):
         B = int(B)
         C = A * B 
         id = r.dbsize() + 1
-        resultats[id] = C
         r.set(id, C) 
 
 
         return " a bien été ajouté au dictionnaire"
 
+connection.close()
