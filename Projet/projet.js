275713d491cb4587df39e719a1060e86e337843b
Borel6 <t.borel33@gmail.com>
2024-03-09 16:57:55 +0100

    mise en forme de l'affichage

--- Projet/projet.js
+++ Projet/projet.js
@@ -14,40 +14,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function displayTweet(){
    
    fetch('http://127.0.0.1:5000/tweets')
